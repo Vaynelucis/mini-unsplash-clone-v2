28de70630d83a0120035ea75d61c5886e74d9db1
Benard Obinna <bernardobinna2@gmail.com>
2019-07-24 06:54:37 +0100

    initial master commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './registerServiceWorker'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./registerServiceWorker";
+import { VueMasonryPlugin } from "vue-masonry";
+import axios from "axios";
+import { VueSpinners } from "@saeris/vue-spinners";
+Vue.use(VueMasonryPlugin, VueSpinners);
+Vue.config.productionTip = false;
 
 new Vue({
+  VueSpinners,
+  VueMasonryPlugin,
+  axios,
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
